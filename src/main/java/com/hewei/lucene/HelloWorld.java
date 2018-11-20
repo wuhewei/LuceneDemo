da3bec0369c830ae7c725c61e97007ebe207fa85
wuhewei <wuhewei1997@qq.com>
2018-11-20 17:09:54 +0800

    已完成HelloWorld程序

--- src/main/java/com/hewei/lucene/HelloWorld.java
+++ src/main/java/com/hewei/lucene/HelloWorld.java
@@ -2,15 +2,28 @@ package com.hewei.lucene;
 
 import com.hewei.utils.File2Documents;
 import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
+import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.search.highlight.*;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
 import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.StringReader;
+import java.nio.file.Paths;
 
- /**
+/**
  * Lucene 入门程序
  *
  * @author hewei
@@ -20,7 +33,7 @@ public class HelloWorld {
 
     private final String FILE_PATH = "D:\\IDEAProject\\LuceneDemo\\src\\main\\resources\\LuceneDataSource\\lucene.txt";
 
-    private final String INDEX_PATH = "D:\\IDEAProject\\LuceneDemo\\src\\main\\resources\\LuceneDataSource\\";
+    private final String INDEX_PATH = "D:\\IDEAProject\\LuceneDemo\\src\\main\\resources\\LuceneIndex\\";
 
 
     /**
@@ -31,23 +44,60 @@ public class HelloWorld {
      */
     @Test
     public void createIndex() throws IOException {
-
-        // file -> document
-        Document doc = File2Documents.file2Document(FILE_PATH);
-
         // 标准语法分词器
         Analyzer analyzer = new StandardAnalyzer();
-
-//        IndexWriter indexWriter = new IndexWriter(/**/);
-//        indexWriter.addDocument(doc);
-
-
+        // 索引存放目录
+        Directory directory = FSDirectory.open(Paths.get(INDEX_PATH));
+        // 索引配置对象
+        IndexWriterConfig config = new IndexWriterConfig(analyzer);
+        // 索引操作对象
+        IndexWriter indexWriter = new IndexWriter(directory, config);
+        // file -> document
+        Document doc = File2Documents.file2Document(FILE_PATH);
+        indexWriter.addDocument(doc);
+        indexWriter.commit();
         // 关闭资源
-//        indexWriter.close();
+        indexWriter.close();
     }
 
     @Test
-    public void search(){
-        String queryString = "lucene";
+    public void search() throws Exception {
+        String queryString = "core";
+        Directory directory = FSDirectory.open(Paths.get(INDEX_PATH));
+        DirectoryReader reader = DirectoryReader.open(directory);
+        // 索引搜索器
+        IndexSearcher searcher = new IndexSearcher(reader);
+        Analyzer analyzer = new StandardAnalyzer();
+        QueryParser parser = new QueryParser("content", analyzer);
+        Query query = parser.parse(queryString);
+        long startTime = System.currentTimeMillis();
+        // 搜索10条
+        TopDocs docs = searcher.search(query, 10);
+        // 打印时间、记录数
+        System.out.println("查找“" + queryString + "”用时" + Double.valueOf(System.currentTimeMillis() - startTime) / 1000 + "秒");
+        System.out.println("查询到" + docs.totalHits + "条记录");
+
+        //加入高亮显示的
+        SimpleHTMLFormatter simpleHTMLFormatter = new SimpleHTMLFormatter("<b><font color=red>", "</font></b>");
+        //计算查询结果最高的得分
+        QueryScorer scorer = new QueryScorer(query);
+        //根据得分算出一个片段
+        Fragmenter fragmenter = new SimpleSpanFragmenter(scorer);
+        Highlighter highlighter = new Highlighter(simpleHTMLFormatter, scorer);
+        //设置显示高亮的片段
+        highlighter.setTextFragmenter(fragmenter);
+
+        for (ScoreDoc scoreDoc : docs.scoreDocs) {
+            Document doc = searcher.doc(scoreDoc.doc);
+            String content = doc.get("content");
+            if (content != null) {
+                TokenStream tokenStream = analyzer.tokenStream("content", new StringReader(content));
+                String summary = highlighter.getBestFragment(tokenStream, content);
+                System.out.println(summary);
+            }
+        }
+
+        // 关闭资源
+        reader.close();
     }
 }
--- src/main/java/com/hewei/utils/File2Documents.java
+++ src/main/java/com/hewei/utils/File2Documents.java
@@ -2,9 +2,10 @@ package com.hewei.utils;
 
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
+import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.IndexOptions;
 
-import java.io.File;
+import java.io.*;
 
 /**
  * @author hewei
@@ -15,10 +16,29 @@ public class File2Documents {
     public static Document file2Document(String path){
         File file = new File(path);
 
+        // 文档信息包含：文件名（标题）、内容、路径、大小
         Document doc = new Document();
-//        doc.add(new Field("", ""), Field.Store.YES, IndexOptions.);
+        doc.add(new TextField("fileName", file.getName(), Field.Store.YES));
+        doc.add(new TextField("content", readToString(file), Field.Store.YES));
+        doc.add(new TextField("filePath", file.getAbsolutePath(), Field.Store.YES));
+        doc.add(new TextField("fileSize", String.valueOf(file.length()), Field.Store.YES));
 
-        return null;
+        return doc;
+    }
+
+    private static String readToString(File file) {
+        StringBuffer buffer = new StringBuffer();
+        try {
+            BufferedReader buff = new BufferedReader(new FileReader(file));
+            String line = null;
+            while ((line = buff.readLine()) != null) {
+                buffer.append(line + "\n");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return buffer.toString();
     }
 
     public static File Document2File(Document document) {
